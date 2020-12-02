a6a0386d52785fb8c8dc33d1c5c511e3e47e902d
Ruben Bañuelos <krankes.hirn@gmail.com>
2020-12-02 16:14:24 -0600

    Coded day 2

---
+++ day2/day2.py
@@ -0,0 +1,23 @@
+#Advent of code 2020 Day 2
+
+import re
+
+f = open("input", "r")
+
+total = 0
+
+for a in f:
+	line = re.split(":",a)
+	restriction = line[0]
+	password    = line[1][1:]
+
+	password.strip
+
+	low  = int(re.split("-",(re.split(" ",restriction)[0]))[0])
+	high = int(re.split("-",(re.split(" ",restriction)[0]))[1])
+	buchstabe = re.split(" ",restriction)[1]
+
+	if (password[low-1] == buchstabe)^(password[high-1] == buchstabe):
+		total += 1
+
+print(total)
